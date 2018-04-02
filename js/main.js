ca7c032367379731b573a1ef6274faa3a50af971
Faith Wallace <fwallacephd@gmail.com>
2018-04-02 11:45:41 -0400

    game board images 040218

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 //Global variables
-var player1 = "";
-var player2 = "";
+var player1;
+var player2;
 var playerPick;
 var firstMove;
-var state = "player1";
+var state = "blackwidow";
+var player1Score;
+var player2Score;
+
 
 //Hide the game before settings
 $(".play-display-area").hide();
@@ -11,13 +14,13 @@ $(".game-board").hide();
 $(".play-again").hide();
 
 //Choose your player
-$(".player-black-widow-button ").on("click", function(){
+$(".player-black-widow-button").on("click", function(){
  player1 = "blackWidow";
  player2 = "hawkeye";
  console.log(player1, player2)
 });
 
-$(".player-hawkeye-button ").on("click", function(){
+$(".player-hawkeye-button").on("click", function(){
  player1 = "hawkeye";
  player2 = "blackWidow";
  console.log(player1, player2)
@@ -41,4 +44,95 @@ $(".play-button").on("click", function(){
  $(".play-display-area").show();
  $(".game-board").show();
  $(".play-again").show();
+});
+
+$("#pos1").on("click", function() {
+ if (state === "blackwidow") {
+  $("#pos1").attr("src", "images/black_widow.png")
+  state = "hawkeye";
+ } else if (state === "hawkeye") {
+  $("#pos1").attr("src", "images/hawkeye.png")
+  state = "blackwidow";
+ }
+});
+
+$("#pos2").on("click", function() {
+ if (state === "blackwidow") {
+  $("#pos2").attr("src", "images/black_widow.png")
+  state = "hawkeye";
+ } else if (state === "hawkeye") {
+  $("#pos2").attr("src", "images/hawkeye.png")
+  state = "blackwidow";
+ }
+});
+
+$("#pos3").on("click", function() {
+ if (state === "blackwidow") {
+  $("#pos3").attr("src", "images/black_widow.png")
+  state = "hawkeye";
+ } else if (state === "hawkeye") {
+  $("#pos3").attr("src", "images/hawkeye.png")
+  state = "blackwidow";
+ }
+});
+
+
+$("#pos4").on("click", function() {
+ if (state === "blackwidow") {
+  $("#pos4").attr("src", "images/black_widow.png")
+  state = "hawkeye";
+ } else if (state === "hawkeye") {
+  $("#pos4").attr("src", "images/hawkeye.png")
+  state = "blackwidow";
+ }
+});
+
+$("#pos5").on("click", function() {
+ if (state === "blackwidow") {
+  $("#pos5").attr("src", "images/black_widow.png")
+  state = "hawkeye";
+ } else if (state === "hawkeye") {
+  $("#pos5").attr("src", "images/hawkeye.png")
+  state = "blackwidow";
+ }
+});
+
+$("#pos6").on("click", function() {
+ if (state === "blackwidow") {
+  $("#pos6").attr("src", "images/black_widow.png")
+  state = "hawkeye";
+ } else if (state === "hawkeye") {
+  $("#pos6").attr("src", "images/hawkeye.png")
+  state = "blackwidow";
+ }
+});
+
+$("#pos7").on("click", function() {
+ if (state === "blackwidow") {
+  $("#pos7").attr("src", "images/black_widow.png")
+  state = "hawkeye";
+ } else if (state === "hawkeye") {
+  $("#pos7").attr("src", "images/hawkeye.png")
+  state = "blackwidow";
+ }
+});
+
+$("#pos8").on("click", function() {
+ if (state === "blackwidow") {
+  $("#pos8").attr("src", "images/black_widow.png")
+  state = "hawkeye";
+ } else if (state === "hawkeye") {
+  $("#pos8").attr("src", "images/hawkeye.png")
+  state = "blackwidow";
+ }
+});
+
+$("#pos9").on("click", function() {
+ if (state === "blackwidow") {
+  $("#pos9").attr("src", "images/black_widow.png")
+  state = "hawkeye";
+ } else if (state === "hawkeye") {
+  $("#pos9").attr("src", "images/hawkeye.png")
+  state = "blackwidow";
+ }
 });
